294a9cf5e4952cb682f965c891b6e92ba989e55f
Lev Vidrak <levv@wix.com>
2017-05-16 12:54:24 +0300

    commit #1

---
+++ android/app/src/main/java/com/barcodetest/MainActivity.java
@@ -0,0 +1,49 @@
+package com.barcodetest;
+import android.os.Bundle;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.reactnativecomponent.barcode.RCTCapturePackage;
+
+import com.facebook.react.ReactActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "barcodetest";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MainApplication application = (MainApplication) this.getApplication();
+
+        application.setReactNativeHost(new ReactNativeHost(application) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return Arrays.<ReactPackage>asList(
+                        new MainReactPackage(),
+                        new RCTCapturePackage()    //register Module
+                );
+            }
+
+        });
+
+        super.onCreate(savedInstanceState);
+    }
+}
+
+
---
+++ index.android.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    ActivityIndicator,
+    ProgressBarAndroid,
+    ActivityIndicatorIOS,
+} from 'react-native';
+import Barcode from 'react-native-smart-barcode'
+
+export default class barcodetest extends Component {
+
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center',}}>
+                <Barcode style={{flex: 1, alignSelf: 'stretch', }}
+                         ref={ component => this._barCode = component }
+                         onBarCodeRead={this._onBarCodeRead}/>
+            </View>
+        );
+    }
+
+    _onBarCodeRead = (e) => {
+        console.log(`e.nativeEvent.data.type = ${e.nativeEvent.data.type}, e.nativeEvent.data.code = ${e.nativeEvent.data.code}`)
+        this._stopScan()
+        Alert.alert(e.nativeEvent.data.type, e.nativeEvent.data.code, [
+            {text: 'OK', onPress: () => this._startScan()},
+        ])
+    }
+
+    _startScan = (e) => {
+        this._barCode.startScan()
+    }
+
+    _stopScan = (e) => {
+        this._barCode.stopScan()
+    }
+
+    _renderActivityIndicator() {
+        return ActivityIndicator ? (
+                <ActivityIndicator
+                    style={{position: 'relative', left: 1, top: 1,}}
+                    animating={true}
+                    size={'large'}/>
+            ) : Platform.OS == 'android' ?
+                (
+                    <ProgressBarAndroid
+                        styleAttr={'large'}/>
+
+                ) : (
+                    <ActivityIndicatorIOS
+                        animating={true}
+                        size={'large'}/>
+                )
+    }
+}
+
+AppRegistry.registerComponent('barcodetest', () => barcodetest);
